672d65ea14a670d0ac615d7f4e35fd94e1a36be9
ShampooCodes <suhaibkhan78601@gmail.com>
2026-09-24 15:06:00 +0500

    setup React vite + tailwind project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className='h-screen bg-slate-900 flex items-center justify-center'>
+        <h1 className='text-4xl font-bold text-pink-400'>HI hello</h1>
+      </div>
+    </>
+  )
+}
+export default App
